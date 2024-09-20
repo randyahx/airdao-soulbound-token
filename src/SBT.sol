4129c66ca58ff2a1b631f512deb6b304928ca904
Randy Ang <randyang@Randys-Laptop.local>
2024-09-21 02:21:27 +0800

    remove achievement system

--- src/SBT.sol
+++ src/SBT.sol
@@ -5,31 +5,20 @@ import "./ISBT.sol";
 import "./ISBTMetadata.sol";
 import "./ISBTErrors.sol";
 
-contract SoulBoundToken is Context, ISBT, ISBTMetadata, ISBTErrors {
+contract SBT is Context, ISBT, ISBTMetadata, ISBTErrors {
     string private _name;
     string private _symbol;
-    address private _contractOwner;
 
     mapping(uint256 => address) private _owners;
     mapping(address => uint256) private _balances;
     mapping(uint256 => string) private _tokenURIs;
-    mapping(uint256 => mapping(string => bool)) private _achievements;
 
     uint256 private _tokenIdCounter;
-
-    event AchievementAdded(uint256 indexed tokenId, string achievement);
-    event AchievementRemoved(uint256 indexed tokenId, string achievement);
-
-    modifier onlyContractOwner() {
-        require(_msgSender() == _contractOwner, "SBT: Caller is not the contract owner");
-        _;
-    }
 
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
         _tokenIdCounter = 0;
-        _contractOwner = _msgSender();
     }
 
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
@@ -82,31 +71,5 @@ contract SoulBoundToken is Context, ISBT, ISBTMetadata, ISBTErrors {
 
         emit Minted(to, tokenId);
         return tokenId;
-    }
-
-    function addAchievement(uint256 tokenId, string memory achievement) external onlyContractOwner {
-        require(_owners[tokenId] != address(0), "SBT: Token does not exist");
-        require(!_achievements[tokenId][achievement], "SBT: Achievement already exists");
-
-        _achievements[tokenId][achievement] = true;
-        emit AchievementAdded(tokenId, achievement);
-    }
-
-    function removeAchievement(uint256 tokenId, string memory achievement) external onlyContractOwner {
-        require(_owners[tokenId] != address(0), "SBT: Token does not exist");
-        require(_achievements[tokenId][achievement], "SBT: Achievement does not exist");
-
-        _achievements[tokenId][achievement] = false;
-        emit AchievementRemoved(tokenId, achievement);
-    }
-
-    function hasAchievement(uint256 tokenId, string memory achievement) public view returns (bool) {
-        require(_owners[tokenId] != address(0), "SBT: Token does not exist");
-        return _achievements[tokenId][achievement];
-    }
-
-    function getTokenIdByOwner(address owner) public view returns (uint256) {
-        require(_balances[owner] > 0, "SBT: Owner does not have a token");
-        return _balances[owner];
     }
 }
